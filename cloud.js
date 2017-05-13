b001a8764bab225fd5feff695e5dc157c2fea3be
warlyware <danward@gmail.com>
2017-05-13 16:44:56 -0400

    Add broken cloud code

---
+++ cloud.js
@@ -0,0 +1,23 @@
+// function Cloud() {
+//     this.cloudPieces = [];
+//     this.cloud = new cloudPiece
+//     this.cloudPiece = this.createCloudPiece();
+//     this.numberOfCloudPieces = 4;
+
+//     this.makeCloud() = function() {
+//         for (var i = 0; i <= this.numberOfCloudPieces; i++) {
+//             cloudPieces.push(this.createCloudPiece());
+//         }
+//     }
+
+//     this.createCloudPiece = function() {
+//         var height = this.getRandomNumber(40, 150);
+//         var width = this.getRandomNumber(80, 300);
+
+//         return ellipse(x, y, height, width);
+//     }
+
+//     this.getRandomNumber = function(min, max) {
+//         return Math.floor(Math.random() * (max - min)) + min;
+//     }
+// }
--- main.js
+++ main.js
@@ -1,4 +1,35 @@
+var clouds = [];
+
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
+    colorMode(RGB);
     background(207, 207, 207);
 }
+
+function draw() {
+    colorMode(RGB);
+    background(207, 207, 207, 10);
+    console.log(random());
+    if (random(1) < .01) {
+        createCloud();
+    }
+}
+
+function createCloud() {
+    var x = getRandomNumber(1, window.innerWidth);
+    var y = getRandomNumber(1, window.innerHeight);
+    var height = getRandomNumber(40, 150);
+    var width = getRandomNumber(window.innerWidth * 0.1, window.innerWidth * 0.4);
+    stroke(255);
+    strokeWeight(4);
+    ellipse(x, y, width, height);
+    console.log('cloud, boom!');
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+window.onresize = function() {
+    setup();
+};
